e2bc86cf0e7d9720615c8f5cb07475f7f2c14f52
Bill Vangos <bvangos@bu.edu>
2015-02-07 15:12:13 -0500

    Authentication Server
    Express for node.js; Web server with google authentication listening on
    localhost:8080

---
+++ Authentication/app.js
@@ -0,0 +1,144 @@
+//Node.js Dependecies
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var logger = require('morgan');
+var LocalStrategy = require('passport-local');
+var TwitterStrategy = require('passport-twitter');
+var FacebookStrategy = require('passport-facebook');
+var exphbs = require('express3-handlebars');
+var logger = require('morgan');
+var methodOverride = require('method-override');
+var passport = require('passport');
+var fs = require('fs');
+var googleStrategy = require('passport-google-oauth').OAuth2Strategy; //Authentication
+
+var hbs = exphbs.create({
+    defaultLayout: 'main', 
+});
+
+//App configuration settings
+var app = express();
+	app.set('views', path.join(__dirname, 'views'));
+	app.use(favicon(__dirname + '/public/favicon.ico'));
+	app.use(logger('dev'));
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: false }));
+	app.use(cookieParser());
+	app.use('/public', express.static(__dirname + '/public'));
+	app.use('/', routes);
+	app.use('/users', users);
+	app.use(express.static(path.join(__dirname, '/public')));
+	app.use(passport.initialize());
+	app.use(passport.session());
+    app.use(session({secret:'MySecret'}));
+    app.use(methodOverride('X-HTTP-Method-Override'));
+	app.engine('handlebars', hbs.engine);
+	app.set('views', __dirname + '\\views');
+	app.set('view engine', 'handlebars');
+
+//Google Project Credentials	
+passport.use(new googleStrategy({
+    clientID: '1071634343206-k1udbc79djnhv5rl6vfl9d08onck216p.apps.googleusercontent.com',
+    clientSecret: 'rxOy49TNSrvKz_NSNqtYmmIN',
+    callbackURL: "http://localhost:8080/auth/google/callback"
+	
+},
+function (accessToken, refreshToken, profile, done) {
+
+
+    fs.writeFile(__dirname + "/profile.json", JSON.stringify(profile, null, 4), function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("profile.json was saved!");
+    }
+});  //profile contains all the personal data returned 
+    done(null, profile)
+}
+));
+
+passport.serializeUser(function(user, callback){
+        console.log('serializing user.');
+        callback(null, user.id);
+    });
+
+passport.deserializeUser(function(user, callback){
+       console.log('deserialize user.');
+       callback(null, user.id);
+    });
+	
+	
+	
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+
+app.use(function(req, res, next){
+  var err = req.session.error,
+      msg = req.session.notice,
+      success = req.session.success;
+
+  delete req.session.error;
+  delete req.session.success;
+  delete req.session.notice;
+
+  if (err) res.locals.error = err;
+  if (msg) res.locals.notice = msg;
+  if (success) res.locals.success = success;
+
+  next();
+});
+
+module.exports = app;
+
+var server = app.listen(8080, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Application listening at http://%s:%s', host, port)
+
+})
+
+
+app.get('/', function(req, res){
+  res.render('home', {user: req.user});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Authentication/routes/index.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var fs = require('fs');
+var router = express.Router();
+var passport = require('passport');
+router.use(express.static(__dirname + '/public'));
+module.exports = router;
+router.use(passport.initialize());
+router.use(passport.session());
+
+
+//===============ROUTES=================
+//displays our homepage
+router.get('/', function(req, res){
+  res.render('home', {user: req.user});
+});
+
+//displays our signup page
+router.get('/signin', function(req, res){
+  res.render('signin');
+});
+
+router.get('/auth/google', passport.authenticate('google',{scope: 'https://www.googleapis.com/auth/plus.me https://www.google.com/m8/feeds https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'}));
+
+router.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res, next) {
+    // Successful authentication, redirect home.
+
+	res.redirect('/index.html');
+
+  });
+  
+   
+  
+router.get('/logout', function (req, res) {
+        req.logOut();
+        res.redirect('/');
+    });
+	
+	
+
+//sends the request through our local signup strategy, and if successful takes user to homepage, otherwise returns then to signin page
+router.post('/local-reg', passport.authenticate('local-signup', {
+  successRedirect: '/',
+  failureRedirect: '/signin'
+  })
+);
+
+//sends the request through our local login/signin strategy, and if successful takes user to homepage, otherwise returns then to signin page
+router.post('/login', passport.authenticate('local-signin', { 
+  successRedirect: '/',
+  failureRedirect: '/signin'
+  })
+);
+
+//logs user out of site, deleting them from the session, and returns to homepage
+router.get('/logout', function(req, res){
+  var name = req.user.username;
+  console.log("LOGGIN OUT " + req.user.username)
+  req.logout();
+  res.redirect('/');
+  req.session.notice = "You have successfully been logged out " + name + "!";
+});
+	
