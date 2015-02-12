4131dcc5ac77c84e1b9059053b3a485e59e65591
Bill Vangos <bvangos@bu.edu>
2015-02-12 11:07:26 -0500

    Reverted to using port 8080
    Was changed to 80 on localhost

--- Authentication/app.js
+++ Authentication/app.js
@@ -109,7 +109,7 @@ app.use(function(req, res, next){
 
 module.exports = app;
 
-var server = app.listen(80, function () {
+var server = app.listen(8080, function () {
 
   var host = server.address().address
   var port = server.address().port
